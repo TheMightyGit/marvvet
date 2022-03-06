537d3c5bcd4dab0767d5fae0b9c59aa444b80700
Johnny Marshall <the.mighty.git@gmail.com>
2022-03-06 14:43:47 +0000

    initial

---
+++ pkg/analyzer/analyzer.go
@@ -0,0 +1,46 @@
+package analyzer
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "marvvet",
+	Doc:      "Checks that Marv300 codebases use only whitelisted imports.",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+}
+
+var importWhitelist = map[string]struct{}{
+	`"github.com/TheMightyGit/marv/marvlib"`:   {},
+	`"github.com/TheMightyGit/marv/marvtypes"`: {},
+	`"image"`:     {},
+	`"strings"`:   {},
+	`"math/rand"`: {},
+	`"sort"`:      {},
+	`"strconv"`:   {},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.ImportSpec)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		importSpec := node.(*ast.ImportSpec)
+
+		if _, found := importWhitelist[importSpec.Path.Value]; found {
+			return
+		}
+
+		pass.Reportf(node.Pos(), "import of external package '%s' is not whitelisted", importSpec.Path.Value)
+	})
+
+	return nil, nil
+}
